51129ab1ac3d2963edd07cba60e96b668a689b7c
mark52asus <mark52asus@gmail.com>
2025-01-11 21:54:41 +0800

    hw01 20250111-5

--- student_assignment.py
+++ student_assignment.py
@@ -58,16 +58,20 @@ final_prompt = ChatPromptTemplate.from_messages([
 # 定義 generate_hw01 函數
 def generate_hw01(question):
     llm = initialize_llm()
-    # 建立訊息
-    message = HumanMessage(content=[{"type": "text", "text": question}])
 
     # 建立語言模型鏈
     chain = final_prompt | llm
 
     # 呼叫模型並回傳結果
     response = chain.invoke({"input": question})
-    return response.content
+    # 嘗試解析為 JSON 格式，確保格式正確
+    try:
+        result = json.loads(response.content)
+        formatted_result = json.dumps(result, ensure_ascii=False, indent=4)
+    except json.JSONDecodeError:
+        formatted_result = "無法解析為正確的 JSON 格式"
 
+    return formatted_result
     
 
 
@@ -95,24 +99,7 @@ def demo(question):
             ]
     )
     response = llm.invoke([message])
-    # 嘗試解析為 JSON 格式，確保格式正確
-    try:
-        result = json.loads(response.content)
-        formatted_result = json.dumps(result, ensure_ascii=False, indent=4)
-    except json.JSONDecodeError:
-        formatted_result = "無法解析為正確的 JSON 格式"
 
-    return formatted_result
 
-    return result    
-    #return response
-if __name__ == '__main__':
-    response = generate_hw01('2024年台灣10月紀念日有哪些?')
-    #response = generate_hw02('2024年台灣12月紀念日有哪些?')
-    #question2 = "2024年台灣10月紀念日有哪些?"
-    #question3 = "根據先前的節日清單，這個節日{\"date\": \"10-31\", \"name\": \"蔣公誕辰紀念日\"}是否有在該月份清單？"
-    #response = generate_hw03(question2, question3)
-    #print(response)
-    #response.pretty_print()
-    pprint(response)
+    return response
 
