326f8df5752a9ca58f5e9e6671ab38831d1d086c
mark52asus <mark52asus@gmail.com>
2025-01-11 22:00:28 +0800

    hw01 20050111-6

--- student_assignment.py
+++ student_assignment.py
@@ -102,4 +102,12 @@ def demo(question):
 
 
     return response
-
+if __name__ == '__main__':
+    #response = generate_hw01('2023台灣紀念日有哪些?')
+    response = generate_hw01('2024年台灣1月紀念日有哪些?')
+    #question2 = "2024年台灣10月紀念日有哪些?"
+    #question3 = "根據先前的節日清單，這個節日{\"date\": \"10-31\", \"name\": \"蔣公誕辰紀念日\"}是否有在該月份清單？"
+    #response = generate_hw03(question2, question3)
+    #print(response)
+    #response.pretty_print()
+    pprint(response)
